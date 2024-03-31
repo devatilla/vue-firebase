2bf4ebf8c5db9f0bf003800f649784187bec5aff
Atilla Altun <atilla.altun@ogr.iu.edu.tr>
2024-04-01 00:39:43 +0300

    intial commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </li>
+        <li class="nav-item">
+          <router-link to="/contact" class="nav-link">Contact</router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="d-flex">
+      <router-link to="/login" class="btn btn-outline-success" type="submit">Giris</router-link>
+      <button class="btn btn-outline-danger" type="submit">cikis</button>
+    </div>
+  </nav>
+</template>
